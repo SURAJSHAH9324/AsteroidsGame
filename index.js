f3bec0f82548da151e00ae06a2ea484c07b9c8c4
surajshah77881@gmail.com <surajshah77881@gmail.com>
2023-10-29 13:45:43 +0530

    rotation done

--- index.js
+++ index.js
@@ -5,45 +5,105 @@ const c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-c.fillStyle = "black";
-c.fillRect(0, 0, canvas.width, canvas.height);
-
 class Player {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.rotation = 0;
   }
   draw() {
+    c.save();
+    c.translate(this.position.x, this.position.y);
+    c.rotate(this.rotation);
+    c.translate(-this.position.x, -this.position.y);
+
     c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
-    c.fillStyle = 'red';
+    c.fillStyle = "red";
     c.fill();
 
+    c.beginPath();
     c.moveTo(this.position.x + 30, this.position.y);
     c.lineTo(this.position.x - 10, this.position.y - 10);
     c.lineTo(this.position.x - 10, this.position.y + 10);
-    c.closePath()
+    c.closePath();
 
     c.strokeStyle = "white";
     c.stroke();
+    c.restore();
+  }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
   }
 }
-
 const player = new Player({
   position: { x: canvas.width / 2, y: canvas.height / 2 },
   velocity: { x: 0, y: 0 },
 });
-player.draw();
 
-window.addEventListener('keydown', (event) => {
-    switch (event.code) {
-        case 'KeyW':
-            console.log("W is pressed");
-            break;
-        case 'KeyA':
-            console.log("A is pressed");
-            break;
-        case 'KeyD':
-            console.log("D is pressed");
-            break;
-    }
+const Keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+const SPEED = 4;
+const Rotaional_Speed = 0.05;
+const friction = 0.99;
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  // console.log('animate')
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  if (Keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * SPEED;
+    player.velocity.y = Math.sin(player.rotation) * SPEED;
+  } else if (!Keys.w.pressed) {
+    player.velocity.x *= friction;
+    player.velocity.y *= friction;
+  }
+
+  if (Keys.d.pressed) player.rotation += Rotaional_Speed;
+
+  if (Keys.a.pressed) player.rotation -= Rotaional_Speed;
+}
+animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      console.log("W is pressed");
+      Keys.w.pressed = true;
+      break;
+    case "KeyA":
+      Keys.a.pressed = true;
+      break;
+    case "KeyD":
+      Keys.d.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      console.log("W is pressed");
+      Keys.w.pressed = false;
+      break;
+    case "KeyA":
+      Keys.a.pressed = false;
+      break;
+    case "KeyD":
+      Keys.d.pressed = false;
+      break;
+  }
 });
